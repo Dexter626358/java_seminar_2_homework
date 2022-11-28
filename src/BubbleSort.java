277deab302cefad10cf4fc43c25e610831cd7d1a
Dexter <gusevsergey85@gmail.com>
2022-11-28 18:31:30 +0300

    SQL request

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,5 +1,20 @@
 /*
 Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
  */
+
 public class BubbleSort {
+    public static int[] getSortedArray(int[] array){
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length-1; j++) {
+                if(array[j] > array[j+1]){
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+
+        }
+        return array;
+    }
 }
--- src/SqlRequest.java
+++ src/SqlRequest.java
@@ -3,5 +3,38 @@
 Если значение null, то параметр не должен попадать в запрос.
 Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
  */
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.Objects;
+
 public class SqlRequest {
+    public static void getSQL(String json) throws ParseException {
+        String beginSQL = "select * from students where ";
+        JSONParser jsonParser = new JSONParser();
+        JSONObject obj =  (JSONObject) jsonParser.parse(json);
+        String name = (String) obj.get("name");
+        String country = (String) obj.get("country");
+        String city = (String) obj.get("city");
+        String age = (String) obj.get("age");
+        StringBuilder builder = new StringBuilder(beginSQL);
+        if (!Objects.equals(name, "null")){
+            builder.append(" name=").append(name);
+        }
+        if (!Objects.equals(country, "null")){
+            builder.append( " AND");
+            builder.append(" country=").append(country);
+        }
+        if (!Objects.equals(city, "null")){
+            builder.append( " AND");
+            builder.append(" city=").append(city);
+        }
+        if (!Objects.equals(age, "null")){
+            builder.append( " AND");
+            builder.append(" age=").append(age);
+        }
+        builder.append(";");
+        System.out.println(builder);
+    }
 }
