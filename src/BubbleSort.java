78c21f996f260e0f56dfec80e503c6cf0b608633
Dexter <gusevsergey85@gmail.com>
2022-12-01 18:24:51 +0300

    SQL request final

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,18 @@
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.util.logging.XMLFormatter;
 /*
 Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
  */
 
 public class BubbleSort {
-    public static int[] getSortedArray(int[] array){
+    public static int[] getSortedArray(int[] array) throws IOException {
+        Logger logger = Logger.getLogger(BubbleSort.class.getName());
+        FileHandler fh = new FileHandler("bubblelogger.xml");
+        logger.addHandler(fh);
+        XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(xml);
         int temp;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length-1; j++) {
@@ -11,6 +20,7 @@ public class BubbleSort {
                     temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
+                    logger.info(String.format("произошел обмен %d и %d", array[j], array[j+1]));
                 }
             }
 
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, IOException, FileNotFoundException {
         //int[] array = {3, 5, 10, 1, 23, 12, 45, 0, 200, 79, 23, 46, 123, 1};
-        String jsonString = "{\"name\":\"null\", \"country\":\"null\", \"city\":\"null\", \"age\":\"18\"}";
+        //String jsonString = "{\"name\":\"null\", \"country\":\"null\", \"city\":\"null\", \"age\":\"18\"}";
         //System.out.println(Arrays.toString(array));
         //BubbleSort.getSortedArray(array);
         //System.out.println(Arrays.toString(array));
-        SqlRequest.getSQL(jsonString);
+        //SqlRequest.getSQL(jsonString);
+        //Calculator.calculatorDouble();
     }
 }
